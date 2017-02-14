f7aae32966a86814463d694c46f6cf3cbc7627b9
yang yao <yang@livepay.my>
2017-02-14 10:39:45 +0800

    add example

--- android/src/main/java/my/fin/RNScanDocModule.java
+++ android/src/main/java/my/fin/RNScanDocModule.java
@@ -1,22 +1,63 @@
 
 package my.fin;
 
+import android.net.Uri;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+//import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Promise;
+import android.graphics.Bitmap;
+
+
+import java.io.IOException;
 
 public class RNScanDocModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+  private final ReactApplicationContext mReactContext;
 
   public RNScanDocModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
+    mReactContext = reactContext;
+    reactContext.addActivityEventListener(this);
   }
 
   @Override
   public String getName() {
     return "RNScanDoc";
   }
+
+  @ReactMethod
+  public void scan(String imagePath, String outputPath, Promise promise) {
+    try {
+      Activity currentActivity = getCurrentActivity();
+
+      if (currentActivity == null) {
+//        response = Arguments.createMap();
+//        response.putString("path", Uri.fromFile().toString());
+//        promise.resolve(response);
+        promise.reject("no activity found.")
+        return;
+      }
+
+      if (imagePath == null || imagePath.isEmpty() || outputPath == null || outputPath.isEmpty()) {
+        promise.reject("no imagePath or outputPath was set.")
+        return;
+      }
+
+      Uri uri = Uri.parse(imagePath)
+
+      bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+      getContentResolver().delete(uri, null, null);
+
+
+    } catch (IOException e) {
+      failureCb.invoke(e.getMessage());
+    }
+
+  }
+
+  private void scanWithExceptions()
 }
